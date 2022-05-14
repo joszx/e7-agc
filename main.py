ec2270039379a61db8ea6c1ec7d54242437441bf
joszx <joshua_rockz@hotmail.com>
2022-05-14 18:51:53 +0800

    Add grayscale and bw conversion with cv2

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import cv2 as cv
 import numpy as np
 import os
 import time
-import cv2
 import pytesseract
 #from time import time
 from windowcapture import WindowCapture
 from ctypes import windll
 from PIL import Image
+from parser import Parser
 
-
-# Change the working directory to the folder this script is in.
-# Doing this because I'll be putting the files from each video in their own folder on GitHub
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Make program aware of DIP scaling e.g. monitor 1 - 125%, monitor 2 - 100%
 # Credit: https://stackoverflow.com/questions/40869982/dpi-scaling-level-affecting-win32gui-getwindowrect-in-python
@@ -31,26 +27,34 @@ while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    screenshot = cv2.rectangle(screenshot, (875, 125), (1100, 225), (0,255,0), 2)
-    screenshot = cv2.rectangle(screenshot, (475, 525), (822, 650), (0,255,0), 2)
+    screenshot = cv.rectangle(screenshot, Parser.EQUIPPED_BY_TOP_LEFT, Parser.EQUIPPED_BY_BOT_RIGHT, (0,255,0), 2)
+    screenshot = cv.rectangle(screenshot, Parser.SUBSTAT_TOP_LEFT, Parser.SUBSTAT_BOT_RIGHT, (0,255,0), 2)
 
     # crop screenshot image by height then by width, Y then X
     equip_region =  screenshot[125:225,875:1100]
-    substat_region = screenshot[525:650,475:822]
+    substat_region = screenshot[Parser.SUBSTAT_TOP_LEFT[1]:Parser.SUBSTAT_BOT_RIGHT[1], Parser.SUBSTAT_TOP_LEFT[0]:Parser.SUBSTAT_BOT_RIGHT[0]]
+
+    # turn substat region bw for tesseract better processing
+    substat_region_grayscale = cv.cvtColor(substat_region, cv.COLOR_BGR2GRAY)
+    (thresh, substat_region_bw) = cv.threshold(substat_region_grayscale, 64, 255, cv.THRESH_BINARY_INV)
 
     cv.imshow('Computer Vision', screenshot)
     cv.imshow('CV equip region', equip_region)
     cv.imshow('CV substat region', substat_region)
+    cv.imshow('CV substat region grayscale', substat_region_grayscale)
+    cv.imshow('CV black and white substat region', substat_region_bw)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time.time() - loop_time)))
     loop_time = time.time()
 
     # tesseract ocr code
-    myconfig = r"--psm 3 --oem 0"
-    text = pytesseract.image_to_string(equip_region, config=myconfig, output_type=pytesseract.Output.DICT)
-    print(text)
-
+    myconfig = r"--psm 3 --oem 3"
+    # custom_config = r'-c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz --psm 6'
+    equipped_by_text = pytesseract.image_to_string(equip_region, config=myconfig, output_type=pytesseract.Output.STRING)
+    print(equipped_by_text)
+    substat_text = pytesseract.image_to_string(substat_region_bw, config=myconfig, output_type=pytesseract.Output.STRING)
+    print(substat_text)
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
@@ -58,7 +62,7 @@ while(True):
         cv.destroyAllWindows()
         break
 
-    time.sleep(1)
+    #time.sleep(1)
 
 
 print('Done.')
---
+++ parser.py
@@ -0,0 +1,16 @@
+
+
+class Parser:
+    # properties
+    EQUIPPED_BY_TOP_LEFT = (875, 125)
+    EQUIPPED_BY_BOT_RIGHT = (1100, 225)
+    EQUIPPED_BY_RECT = (EQUIPPED_BY_TOP_LEFT, EQUIPPED_BY_BOT_RIGHT)
+    SUBSTAT_TOP_LEFT = (475, 500)
+    SUBSTAT_BOT_RIGHT = (822, 650)
+    SUBSTAT_RECT = (SUBSTAT_TOP_LEFT, SUBSTAT_BOT_RIGHT)
+
+    # constructor
+    def __init__(self):
+        pass
+
+
