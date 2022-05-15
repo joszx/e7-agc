ff2efce0969ac4c1bd628f7f993bdc30016c506f
joszx <joshua_rockz@hotmail.com>
2022-05-16 01:47:25 +0800

    Add orange filter image preprocessing
    Also defined orange min and max values for filtering.
    Changed tesseract page segmentation mode to single block of text for
    better OCR.

--- test1.py
+++ test1.py
@@ -6,10 +6,6 @@ from windowcapture import WindowCapture
 from ctypes import windll
 from parser import Parser
 
-# Constants
-ORANGE_MIN = (5, 100, 50)
-ORANGE_MAX = (15, 255, 255)
-
 
 # Make program aware of DPI scaling e.g. monitor 1 - 125%, monitor 2 - 100%
 # Credit: https://stackoverflow.com/questions/40869982/dpi-scaling-level-affecting-win32gui-getwindowrect-in-python
@@ -28,29 +24,21 @@ while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    #screenshot = cv.rectangle(screenshot, Parser.EQUIPPED_BY_TOP_LEFT, Parser.EQUIPPED_BY_BOT_RIGHT, (0,255,0), 2)
-    screenshot = cv.rectangle(screenshot, Parser.SUBSTAT_TOP_LEFT, Parser.SUBSTAT_BOT_RIGHT, (255,255,255), 2)
+    screenshot = cv.rectangle(screenshot, Parser.EQUIPPED_BY_TOP_LEFT, Parser.EQUIPPED_BY_BOT_RIGHT, (0,255,0), 2)
+    screenshot = cv.rectangle(screenshot, Parser.SUBSTAT_TOP_LEFT, Parser.SUBSTAT_BOT_RIGHT, (255,255,255), 3)
 
     # crop screenshot image by height then by width, Y then X
-    #equip_region =  screenshot[125:225,875:1100]
+    equip_region =  screenshot[125:225,875:1100]
     substat_region = screenshot[Parser.SUBSTAT_TOP_LEFT[1]:Parser.SUBSTAT_BOT_RIGHT[1], Parser.SUBSTAT_TOP_LEFT[0]:Parser.SUBSTAT_BOT_RIGHT[0]]
 
-    # filter out orange for substat region
-    substat_region_hsv = cv.cvtColor(substat_region, cv.COLOR_BGR2HSV)
-    orange_mask = cv.inRange(substat_region_hsv, ORANGE_MIN, ORANGE_MAX)
-    invert_orange_mask = cv.bitwise_not(orange_mask) # maybe could be replaced with 255 - orange_mask
-
-    substat_region_filtered_orange = cv.bitwise_and(substat_region, substat_region, mask=invert_orange_mask)
-
     # turn substat region bw for tesseract better processing
-    substat_region_grayscale = cv.cvtColor(substat_region_filtered_orange, cv.COLOR_BGR2GRAY)
+    substat_region_grayscale = cv.cvtColor(substat_region, cv.COLOR_BGR2GRAY)
     (thresh, substat_region_bw) = cv.threshold(substat_region_grayscale, 30, 255, cv.THRESH_BINARY_INV)
-    #substat_region_bw = cv.resize(substat_region_bw, None, fx=1.2, fy=1.2, interpolation=cv.INTER_CUBIC)
+    substat_region_bw = cv.resize(substat_region_bw, None, fx=1.2, fy=1.2, interpolation=cv.INTER_CUBIC)
 
     cv.imshow('Computer Vision', screenshot)
-    #cv.imshow('CV equip region', equip_region)
+    cv.imshow('CV equip region', equip_region)
     cv.imshow('CV substat region', substat_region)
-    cv.imshow('CV substat region filter orange', substat_region_filtered_orange)
     cv.imshow('CV substat region grayscale', substat_region_grayscale)
     cv.imshow('CV black and white substat region', substat_region_bw)
 
@@ -59,10 +47,10 @@ while(True):
     loop_time = time.time()
 
     # tesseract ocr code
-    myconfig = r"--psm 6 --oem 3"
+    myconfig = r"--psm 3 --oem 1"
     # custom_config = r'-c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz --psm 6'
-    #equipped_by_text = pytesseract.image_to_string(equip_region, config=myconfig, output_type=pytesseract.Output.STRING)
-    #print(equipped_by_text)
+    equipped_by_text = pytesseract.image_to_string(equip_region, config=myconfig, output_type=pytesseract.Output.STRING)
+    print(equipped_by_text)
     substat_text = pytesseract.image_to_string(substat_region_bw, config=myconfig, output_type=pytesseract.Output.STRING)
     print(substat_text)
 
