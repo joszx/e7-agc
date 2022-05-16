9d218ee368128f738590d411739e52b883c3a70a
joszx <joshua_rockz@hotmail.com>
2022-05-17 02:39:35 +0800

    Move bounding box info to screengrabber class

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pytesseract
 from windowcapture import WindowCapture
 from ctypes import windll
 from parser import Parser
+from screengrabber import ScreenGrabber
 
 # Constants
 ORANGE_MIN = (5, 100, 50)
@@ -29,11 +30,12 @@ while(True):
     screenshot = wincap.get_screenshot()
 
     #screenshot = cv.rectangle(screenshot, Parser.EQUIPPED_BY_TOP_LEFT, Parser.EQUIPPED_BY_BOT_RIGHT, (0,255,0), 2)
-    screenshot = cv.rectangle(screenshot, Parser.SUBSTAT_TOP_LEFT, Parser.SUBSTAT_BOT_RIGHT, (255,255,255), 2)
+    screenshot = cv.rectangle(screenshot, ScreenGrabber.SUBSTAT_TOP_LEFT, ScreenGrabber.SUBSTAT_BOT_RIGHT, (255,255,255), 2)
+    screenshot = cv.rectangle(screenshot, ScreenGrabber.GEAR_INFO_TOP_LEFT, ScreenGrabber.GEAR_INFO_BOT_RIGHT, (255,255,255), 2)
 
     # crop screenshot image by height then by width, Y then X
     #equip_region =  screenshot[125:225,875:1100]
-    substat_region = screenshot[Parser.SUBSTAT_TOP_LEFT[1]:Parser.SUBSTAT_BOT_RIGHT[1], Parser.SUBSTAT_TOP_LEFT[0]:Parser.SUBSTAT_BOT_RIGHT[0]]
+    substat_region = screenshot[ScreenGrabber.SUBSTAT_TOP_LEFT[1]:ScreenGrabber.SUBSTAT_BOT_RIGHT[1], ScreenGrabber.SUBSTAT_TOP_LEFT[0]:ScreenGrabber.SUBSTAT_BOT_RIGHT[0]]
 
     # filter out orange for substat region
     substat_region_hsv = cv.cvtColor(substat_region, cv.COLOR_BGR2HSV)
@@ -47,12 +49,27 @@ while(True):
     (thresh, substat_region_bw) = cv.threshold(substat_region_grayscale, 30, 255, cv.THRESH_BINARY_INV)
     #substat_region_bw = cv.resize(substat_region_bw, None, fx=1.2, fy=1.2, interpolation=cv.INTER_CUBIC)
 
+    # get gear info details
+    gear_info_region = screenshot[ScreenGrabber.GEAR_INFO_TOP_LEFT[1]:ScreenGrabber.GEAR_INFO_BOT_RIGHT[1], ScreenGrabber.GEAR_INFO_TOP_LEFT[0]:ScreenGrabber.GEAR_INFO_BOT_RIGHT[0]]
+    
+    gear_info_region_hsv = cv.cvtColor(gear_info_region, cv.COLOR_BGR2HSV)
+    gear_info_orange_mask = cv.inRange(gear_info_region_hsv, ORANGE_MIN, ORANGE_MAX)
+    gear_info_invert_orange_mask = cv.bitwise_not(gear_info_orange_mask) # maybe could be replaced with 255 - orange_mask
+    gear_info_filtered_orange = cv.bitwise_and(gear_info_region, gear_info_region, gear_info_invert_orange_mask)
+
+    gear_info_grayscale = cv.cvtColor(gear_info_region, cv.COLOR_BGR2GRAY)
+    (thresh, gear_info_bw) = cv.threshold(gear_info_grayscale, 100, 255, cv.THRESH_BINARY_INV)
+
     cv.imshow('Computer Vision', screenshot)
     #cv.imshow('CV equip region', equip_region)
-    cv.imshow('CV substat region', substat_region)
-    cv.imshow('CV substat region filter orange', substat_region_filtered_orange)
-    cv.imshow('CV substat region grayscale', substat_region_grayscale)
+    # cv.imshow('CV substat region', substat_region)
+    # cv.imshow('CV substat region filter orange', substat_region_filtered_orange)
+    # cv.imshow('CV substat region grayscale', substat_region_grayscale)
     cv.imshow('CV black and white substat region', substat_region_bw)
+    cv.imshow('CV gear info region', gear_info_region)
+    cv.imshow('Cv gear info filtered orange', gear_info_orange_mask)
+    cv.imshow('CV gear info grayscale',gear_info_grayscale)
+    cv.imshow('CV gear info bw', gear_info_bw)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time.time() - loop_time)))
@@ -65,6 +82,7 @@ while(True):
     #print(equipped_by_text)
     substat_text = pytesseract.image_to_string(substat_region_bw, config=myconfig, output_type=pytesseract.Output.STRING)
     print(substat_text)
+    #gearinfo_text = pytesseract.image_to_string()
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
@@ -72,7 +90,7 @@ while(True):
         cv.destroyAllWindows()
         break
 
-    time.sleep(1)
+    #time.sleep(1)
 
 
 print('Done.')
--- parser.py
+++ parser.py
@@ -1,13 +1,6 @@
 
 
 class Parser:
-    # properties
-    EQUIPPED_BY_TOP_LEFT = (875, 125)
-    EQUIPPED_BY_BOT_RIGHT = (1100, 225)
-    EQUIPPED_BY_RECT = (EQUIPPED_BY_TOP_LEFT, EQUIPPED_BY_BOT_RIGHT)
-    SUBSTAT_TOP_LEFT = (35, 400)
-    SUBSTAT_BOT_RIGHT = (445, 530)
-    SUBSTAT_RECT = (SUBSTAT_TOP_LEFT, SUBSTAT_BOT_RIGHT)
 
     # constructor
     def __init__(self):
