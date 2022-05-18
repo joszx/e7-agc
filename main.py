027c32f5ba24331b5373ea8417efddddb833a801
joszx <joshua_rockz@hotmail.com>
2022-05-18 23:30:35 +0800

    Modify gear enhance bounding box size

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ while(True):
     screenshot = screengrabber.make_rectangle_around(screenshot, "substat text")
     screenshot = screengrabber.make_rectangle_around(screenshot, "substat roll")
     #screenshot = screengrabber.make_rectangle_around(screenshot, "gear level")
+    #screenshot = screengrabber.make_rectangle_around(screenshot, "gear enhance")
 
     # crop screenshot image by height then by width, Y then X
     substat_text_region = screengrabber.crop_image_around(screenshot, "substat text")
@@ -48,14 +49,16 @@ while(True):
 
     # get gear info details
     gear_level_region = screengrabber.crop_image_around(screenshot, "gear level")
+    gear_enhance_region = screengrabber.crop_image_around(screenshot, "gear enhance")
 
     gear_level_bw = screengrabber.transform_image_bw(gear_level_region, 175)
+    gear_enhance_bw = screengrabber.transform_image_bw(gear_enhance_region, 200)
 
     cv.imshow('Computer Vision', screenshot)
     cv.imshow('CV substat roll region bw', substat_roll_region_bw)
     cv.imshow('CV black and white substat region', substat_text_region_bw)
-    cv.imshow('CV gear level region', gear_level_region)
     cv.imshow('CV gear level bw', gear_level_bw)
+    cv.imshow('CV gear enhance bw', gear_enhance_bw)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time.time() - loop_time)))
@@ -63,8 +66,9 @@ while(True):
 
     # tesseract ocr code
     substat_text_config = r"--psm 6 --oem 3"
-    substat_roll_config = r"-c tessedit_char_whitelist='1234567890%' --psm 6 --oem 0" # tesseract only as LSTM detects 11 as 1
+    substat_roll_config = r"-c tessedit_char_whitelist='1234567890%' --psm 6 --oem 3" # LSTM detects 11 as 1 while tesseract detects % as 0 0
     gear_level_config = r'-c tessedit_char_whitelist=1234567890 --psm 8 --oem 3'
+    gear_enhance_config = r"-c tessedit_char_whitelist='1234567890' --psm 8 --oem 3"
     # custom_config = r'-c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz --psm 6'
     #equipped_by_text = pytesseract.image_to_string(equip_region, config=substat_text_config, output_type=pytesseract.Output.STRING)
     #print(equipped_by_text)
@@ -74,7 +78,8 @@ while(True):
     print(substat_roll)
     gear_level_text = pytesseract.image_to_string(gear_level_bw, config=gear_level_config, output_type=pytesseract.Output.STRING)
     print(gear_level_text)
-
+    gear_enhance_text = pytesseract.image_to_string(gear_enhance_bw, config=gear_enhance_config, output_type=pytesseract.Output.STRING)
+    print(gear_enhance_text)
 
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
